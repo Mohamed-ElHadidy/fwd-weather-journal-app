8e18636c527d84c2ccbce75d98222df0f72068bd
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2020-06-17 22:01:59 +0200

    feat: added validations for inputs

--- website/app.js
+++ website/app.js
@@ -3,6 +3,7 @@ const zipCode = document.getElementById('zip');
 const feelings = document.getElementById('feelings');
 const generate = document.getElementById('generate');
 const userData = {};
+let errors = '';
 
 console.log(zipCode.value, feelings.value);
 // Create a new date instance dynamically with JS
@@ -35,13 +36,30 @@ const postData = async (url = '', data = {}) => {
 // inputs click event
 generate.addEventListener('click', (e) => {
     console.log(e);
-    
-    // set the input data to post them
-    userData.zipCode = zipCode.value;
-    userData.feelings = feelings.value,
 
-    console.log(userData);
-    // post data to server
-    postData('/addPost', userData);
-    
+    // validation casses 
+    // wrong zip code or empty and empty feelings
+    // wrong zip code and valid feelings
+    // valid zip code and empty feelings
+    // valid zip code and feelings
+
+    if (feelings.value.trim().length < 3 && (zipCode.value == '' || isNaN(Number(zipCode.value))) ) {
+        errors = 'Please add a valid zip/postal code number and describe your feeling';
+        console.log(errors)
+    } else if ((zipCode.value == '') || (isNaN(Number(zipCode.value)))) {
+        errors = 'Please enter a valid zip/postal code number';
+        console.log(errors)
+    } else if (feelings.value.trim().length < 3) {
+        errors = 'Please describe your feeling';
+        console.log(errors)
+    } else {
+        // set the input data to post them
+        userData.zipCode = zipCode.value;
+        userData.feelings = feelings.value,
+
+        console.log(userData);
+        // post data to server
+        postData('/addPost', userData);
+    }
+
 });
