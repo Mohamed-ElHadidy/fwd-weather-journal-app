f50125ccb778bdfb92e5c557aa33cadaa51f9294
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2020-06-19 21:28:36 +0200

    feat: added fetching data from openweathermap api

--- website/app.js
+++ website/app.js
@@ -13,10 +13,10 @@ const generate = document.getElementById('generate');
 const userData = {};
 let errors = '';
 
-console.log(zipCode.value,country.value, feelings.value);
+console.log(zipCode.value, country.value, feelings.value);
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = `${d.getMonth() + 1}  .  ${d.getDate()}  .  ${d.getFullYear()}`;
 
 
 const postData = async (url = '', data = {}) => {
@@ -41,9 +41,11 @@ const postData = async (url = '', data = {}) => {
     }
 }
 
-// inputs click event
-generate.addEventListener('click', (e) => {
-    console.log(e);
+// inputs validation before fetching temprature data from openweathermap api
+const checkInvalidity = () => {
+    const invalid_zipCode = zipCode.value == '' || isNaN(Number(zipCode.value));
+    const invalid_feeling = feelings.value.trim().length < 3;
+    const ivalid_countryCode = (country.value.trim().length != 2 || country.value.match(/[0-9]/g) != null);
 
     // validation casses 
     // wrong zip code or empty, invalid country code or empty and empty feelings
@@ -53,11 +55,6 @@ generate.addEventListener('click', (e) => {
     // invalid  country code , valid zip code and feelings
     // invalid  feelings , valid  country codezip code
     // valid zip code and feelings
-
-    const invalid_zipCode = zipCode.value == '' || isNaN(Number(zipCode.value));
-    const invalid_feeling = feelings.value.trim().length < 3;
-    const ivalid_countryCode = (country.value.trim().length != 2 || country.value.match(/[0-9]/g) != null);
-
 
     if (invalid_feeling && ivalid_countryCode && (invalid_zipCode)) {
         errors = 'Please add a valid zip/postal code number, two letters country code and describe your feeling';
@@ -78,14 +75,45 @@ generate.addEventListener('click', (e) => {
         errors = 'Please describe your feeling';
         console.log(errors)
     } else {
-        // set the input data to post them
-        userData.zipCode = zipCode.value;
-        userData.country = country.value,
-        userData.feelings = feelings.value,
-
-        console.log(userData);
-        // post data to server
-        postData('/addPost', userData);
+        errors = '';
     }
+}
 
-});
+// fetching temprature data from openweathermap api
+const getWeatherData = async (url = '', data = {}) => {
+
+    // inputs validation before fetching temprature data from openweathermap api
+    checkInvalidity();
+
+    // if all inputs data are valid > fetching temprature data
+    if (errors == '') {
+        
+        const Full_URL = `${API_base}zip=${zipCode.value},${(country.value).toLowerCase()}&appid=${API_key}`;
+       
+        const response = await fetch(Full_URL);
+
+        try {
+            const newData = await response.json();
+            console.log(newData);
+
+             // set the input data to post them
+            userData.zipCode = zipCode.value;
+            userData.country = country.value;
+            userData.feelings = feelings.value;
+            userData.temperature = `${newData.main.temp - 273.15} °C`;
+            userData.date = newDate;
+
+            console.log(userData);
+            // post data to server
+            postData('/addPost', userData);
+
+            return newData;
+
+        } catch (error) {
+            console.log("error", error);
+        }
+    }
+}
+
+// inputs click event
+generate.addEventListener('click', getWeatherData)
